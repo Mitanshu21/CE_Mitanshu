97a976a1d58931e1efc5d5b95214233b8b768841
mitanshu <mitanshu2531@gmail.com>
2020-07-31 06:53:36 +0530

    Add files via upload

---
+++ LAB_1/2.add_two_num_usr_input.cs
@@ -0,0 +1,17 @@
+using System;
+namespace ConsoleApplication2
+{
+	class program
+	{
+		static void Main(string[] args)
+		{
+            		Console.WriteLine("Enter First number:- ");
+			string num1=Console.ReadLine();
+            		Console.WriteLine("Enter Seccond number:- ");
+            		string num2=Console.ReadLine();
+			int n1=Int32.Parse(num1);
+			int n2=Int32.Parse(num2);
+			Console.WriteLine("Ans is {0}",n1+n2);
+		}
+	}
+}
