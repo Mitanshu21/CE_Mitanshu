d1f0641662a7336e6611a1a4ee6ef1443f77c309
mitanshu <mitanshu2531@gmail.com>
2020-10-03 19:50:29 +0530

    Add files via upload

---
+++ LAB_8/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+using System.Text;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Calcadd _calcadd;
+
+        public HomeController(Calcadd calcadd)
+        {
+            _calcadd = calcadd;
+        }
+        public string Index()
+        {
+            return "Hello From Index";
+        }
+
+        public ViewResult test1()
+        {
+            Calc c = _calcadd.Add();
+            ViewBag.calc = c;
+            return View(c);
+        }
+        [HttpGet]
+        public ActionResult test2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult test2_ans (string num1,string num2)
+        {
+            Calc c = new Calc();
+            c.answer = Int32.Parse(num1) + Int32.Parse(num2);
+            return View(c);
+        }
+    }
+}
---
+++ LAB_8/Models/Calcadd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Calcadd
+    {
+        Calc c;
+        public Calcadd()
+        {
+            c = new Calc();
+            c.num1 = 10;
+            c.num2 = 12;
+        }
+
+        public Calcadd(Calc c)
+        {
+            this.c = c;
+        }
+        public Calc Add()
+        {
+            c.answer = c.num1 + c.num2;
+            return c;
+        }
+    }
+}
